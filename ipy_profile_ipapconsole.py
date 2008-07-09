f6008b98114b14b6d26acfb9436d095a8f4b5b1f
gcuni <gcuni@cells.es>
2008-07-09 15:53:40 +0000

    added support for 'sendfw' and 'listversions' magic functions

--- ipy_profile_ipapconsole.py
+++ ipy_profile_ipapconsole.py
@@ -1,4 +1,5 @@
 import sys
+import array
 import IPython
 from pyIcePAP import *
 
@@ -15,17 +16,18 @@ def main():
         self.ice.connect(shouldReconnect=False)
     ip.expose_magic("connect",connect)
 
-    def w(self,parameter_s='',name="cmd"):
+    def w(self,parameter_s='',name="w"):
         command = parameter_s.upper()
         command = command.replace("\\","")
         print "-> "+command
         try:
             self.ice.sendWriteCommand(command)
-        except IcePAPException,e:
+        except Exception,e:
             print "!<- Some exception occurred: ",e
+            return e
     ip.expose_magic("w",w)
 
-    def wr(self,parameter_s='',name="cmd"):
+    def wr(self,parameter_s='',name="wr"):
         command = parameter_s.upper()
         command = command.replace("\\","")
         print "-> "+command
@@ -33,8 +35,72 @@ def main():
             #print "<- "+self.ice.sendWriteReadCommand(command)
             ans = self.ice.sendWriteReadCommand(command)
             return ans
-        except IcePAPException,e:
+        except Exception,e:
+            print "!<- Some exception occurred: ",e
             return e
     ip.expose_magic("wr",wr)
+
+
+    def sendfw(self,parameter_s='',name="sendfw"):
+        try:
+            filename = parameter_s
+            f = file(filename,'rb')
+            data = f.read()
+            data = array.array('H',data)
+            f.close()
+            nworddata = (len(data))
+            chksum = sum(data)
+            print "File size: %d bytes, cheksum %d (%s)" % (len(data),chksum,hex(chksum))
+
+            print "Setting MODE PROG"
+            cmd = "#MODE PROG"
+            answer = wr(cmd)
+            print answer
+        
+            print "Transferring firmware"
+            cmd = "*PROG SAVE"
+            w(cmd)
+            
+            startmark = 0xa5aa555a
+            maskedchksum = chksum & 0xffffffff
+            self.ice.sendData(struct.pack('L',startmark))
+            self.ice.sendData(struct.pack('L',nworddata))
+            self.ice.sendData(struct.pack('L',maskedchksum))
+            
+            #self.ice.sendData(data.tostring())
+            for i in range(len(data)):
+                self.ice.sendData(struct.pack('H',data[i]))
+            time.sleep(7)
+            print "Remember Icepap system is in MODE PROG"
+            return wr("?MODE")
+        except Exception,e:
+            print "!<- Some exception occurred: ",e
+            return e
+    ip.expose_magic("sendfw",sendfw)
+
+
+    def listversions(self,parameter_s='',name="listversions"):
+        versions_dict = {}
+        ver_cmd = "VER"
+        if parameter_s != '':
+            ver_cmd = parameter_s.upper()
+        sys_status = self.ice.sendWriteReadCommand("?SYSSTAT")
+        sys_status = sys_status[sys_status.index("0x"):]
+        sys_status = int(sys_status,16)
+        for rack in range(16):
+            if (sys_status & (1<<rack)) > 0:
+                version = self.ice.sendWriteReadCommand("%d:?%s"%(rack*10,ver_cmd))
+                versions_dict[rack*10] = version
+                rack_status = self.ice.sendWriteReadCommand("?SYSSTAT %d"%rack)
+                rack_status = rack_status[rack_status.index("0x"):]
+                rack_status = int(rack_status.split(" ")[1],16)
+                for driver in range(8):
+                    if(rack_status & (1<<driver)) > 0:
+                        addr = (rack*10+driver+1)
+                        version = self.ice.sendWriteReadCommand("%d:?%s"%(addr,ver_cmd))
+                        versions_dict[addr] = version
+        return versions_dict
+    ip.expose_magic("listversions",listversions)
+        
 
 main()
