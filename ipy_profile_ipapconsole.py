104a27044e373821c60bfbf2293a9ef4ed60680f
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2011-11-10 16:00:12 +0000

    added comments when upgrading firmware from ipython consoel
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/pyIcePAP@10740 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ipy_profile_ipapconsole.py
+++ ipy_profile_ipapconsole.py
@@ -110,4 +110,30 @@ def main():
     ip.expose_magic("listversions",listversions)
 
 
+# ALL THIS INFO IS IN RT 13748
+# https://rt.cells.es/Ticket/Display.html?id=13748
+#
+# IT WOULD BE VERY USEFUL TO HAVE SOME MAGIC COMANDS TO:
+# - SAVE POSITION REGS
+# - COMPARE THEM
+# - AND RESET THEM IF THEY DIFFER (USING A CONFIRMATION I/O OPERATION)
+#
+# THE IDEA WOULD BE TO CONVERT THE OUTPUT OF THE FOLLOWING PROCEDURE
+# AS A DICTIONARY (using wro), RETURN IT, AND BE ABLE TO DO COMPARISONS
+#
+# ANOTHER FEATURE TO BE CHECKED AFTER AN UPGRADE IS THE DISDIS STATUS
+
+#for d in range(1,7):
+#    print('--------------------------------------------------')
+#    wr $d:?indexer
+#    wr $d:?cfg possrc
+#    wr $d:?cfg tgtenc
+#    for reg in ['','INDEXER','ENCIN','INPOS','ABSENC','MOTOR','TGTENC','SHFTENC']:
+#        wr $d:?pos $reg
+#        wr $d:?enc $reg
+#        print('--------------------------------------------------')
+                
+
+# NOTE: %wr can be called by: _ip.magic('wr ...') or ipmagic('wr ...')
+
 main()
