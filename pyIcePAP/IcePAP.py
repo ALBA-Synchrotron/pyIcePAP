0c3d6bbf5fb0dac0472bf06f4a8c934b733c48d9
cpascual <cpascual@cells.es>
2017-10-31 10:28:47 +0100

    Add license info (GPL)
    Add license notice headers and LICENSE.txt file

--- pyIcePAP/IcePAP.py
+++ pyIcePAP/IcePAP.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------------
+# This file is part of pyIcePAP (https://github.com/ALBA-Synchrotron/pyIcePAP)
+#
+# Copyright 2008-2017 CELLS / ALBA Synchrotron, Bellaterra, Spain
+#
+# Distributed under the terms of the GNU General Public License,
+# either version 3 of the License, or (at your option) any later version.
+# See LICENSE.txt for more info.
+# ------------------------------------------------------------------------------
+
+
 # PyIcepap for Icepap firmware version 1.16
 try: import serial
 except: pass
