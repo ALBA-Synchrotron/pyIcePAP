812ef3b04ccf623e0c7fefe15a56bdf5de3191cf
Jordi Andreu <jandreu@cells.es>
2018-02-14 15:01:38 +0100

    Expose new class at module level.
    Expose the EthIcePAPController class at the module level.

--- pyIcePAP/__init__.py
+++ pyIcePAP/__init__.py
@@ -8,7 +8,9 @@
 # See LICENSE.txt for more info.
 # -----------------------------------------------------------------------------
 
+# TODO: replace implicit imports by explicit using the __all__ = [...] structure
 from IcePAP_Eth import *
 from IcePAP_Serial import *
 from IcePAP import *
 from icepapdef import *
+from .controller import EthIcePAPController
