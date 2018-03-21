896a0f847ec537ef38d9dbcc1aaccaf4ef184bdc
Roberto Javier Homs Puron <rhoms@cells.es>
2018-03-21 15:41:52 +0100

    Implement update command

--- pyIcePAP/__main__.py
+++ pyIcePAP/__main__.py
@@ -14,8 +14,11 @@
 
 import argparse
 import logging
+import time
+import os
 from .backups import IcePAPBackup
 from .communication import EthIcePAPCommunication
+from .programming import firmware_update
 
 
 def main():
@@ -33,7 +36,8 @@ def main():
     save_cmd.add_argument('host', help='IcePAP Host')
     save_cmd.add_argument('-p', '--port', default=5000, help='IcePAP port')
     save_cmd.add_argument('-t', '--timeout', default=3, help='Socket timeout')
-    save_cmd.add_argument('filename', help='Output file name')
+    save_cmd.add_argument('--bkpfile', help='Output backup filename',
+                            default='')
     save_cmd.add_argument('axes', nargs='*', help='Axes to save, default all',
                           type=int, default=[])
     save_cmd.add_argument('-d', '--debug', action='store_true',
@@ -58,6 +62,25 @@ def main():
     # -------------------------------------------------------------------------
     #                           Firmware commands
     # -------------------------------------------------------------------------
+    # Update
+    update_cmd = subps.add_parser('update', help='Command to change the '
+                                                 'firmware version. It '
+                                                 'creates a backup before to '
+                                                 'change the FW')
+    update_cmd.set_defaults(which='update')
+    update_cmd.add_argument('host', help='IcePAP Host')
+    update_cmd.add_argument('-p', '--port', default=5000, help='IcePAP port')
+    update_cmd.add_argument('-t', '--timeout', default=3,
+                            help='Socket timeout')
+    update_cmd.add_argument('--bkpfile', help='Output backup filename',
+                            default='')
+    update_cmd.add_argument('--no-check',
+                            help='Avoid the checking procedure after the '
+                                 'update',
+                            dest='nocheck')
+    update_cmd.add_argument('fwfile', help='Firmware binary file')
+    update_cmd.add_argument('-d', '--debug', action='store_true',
+                            help='Activate log level DEBUG')
 
     # -------------------------------------------------------------------------
     #                           IcePAP commands
@@ -86,8 +109,14 @@ def main():
                         format='%(asctime)s - %(name)s - %(levelname)s - '
                                '%(message)s')
     if args.which == 'save':
+        if args.bkpfile == '':
+            value = time.strftime('%Y%m%d_%H%m%S')
+            args.bkpfile = '{0}_icepap_backup.cfg'.format(value)
+        abspath = os.path.abspath(args.bkpfile)
+        print('Saving backup on: {0}\n'.format(abspath))
+
         ipap_bkp = IcePAPBackup(args.host, args.port, args.timeout)
-        ipap_bkp.do_backup(args.filename, args.axes)
+        ipap_bkp.do_backup(abspath, args.axes)
     elif args.which == 'check':
         ipap_bkp = IcePAPBackup(host=args.host, cfg_file=args.filename)
         ipap_bkp.do_check(args.axes)
@@ -95,6 +124,32 @@ def main():
         ipap_com = EthIcePAPCommunication(host=args.host, port=args.port,
                                           timeout=args.timeout)
         print(ipap_com.send_cmd(args.command))
+    elif args.which == 'update':
+        if args.bkpfile == '':
+            value = time.strftime('%Y%m%d_%H%m%S')
+            args.bkpfile = '{0}_icepap_backup.cfg'.format(value)
+        abspath = os.path.abspath(args.bkpfile)
+        print('\nUpdating {0} with firmware file {1}'.format(args.host,
+                                                           args.fwfile))
+        print('Saving backup on: {0}\n'.format(abspath))
+        ipap_bkp = IcePAPBackup(host=args.host, port=args.port,
+                                timeout=args.timeout)
+        ipap_bkp.do_backup(abspath)
+        print('*' * 80)
+        if firmware_update(args.host, args.fwfile):
+            print('Restore active drivers...')
+            ipap_bkp = IcePAPBackup(cfg_file=abspath)
+            ipap_bkp.active_axes()
+            if not args.nocheck:
+                print('Checking registers....')
+                ipap_bkp.do_check()
+        else:
+            print('Errors on update firmware. Skipping active and check '
+                  'driver procedure.')
+        print('*' * 80)
+        print('\nUpdated {0} with firmware file {1}'.format(args.host,
+                                                       args.fwfile))
+        print('Saved backup on: {0}\n'.format(abspath))
 
 
 if __name__ == '__main__':
