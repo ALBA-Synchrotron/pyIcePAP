145eb388b929b8ce8136d2d661eec0a01390d9c8
Roberto Javier Homs Puron <rhoms@cells.es>
2018-06-21 16:44:43 +0200

    Bump version 2.3.4 to 2.3.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '2.3.4' 
+__version = '2.3.5' 
 
 # windows installer:
 # python setup.py bdist_wininst
