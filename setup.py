0cccae1ed6f8a537b5fc91730e6b80c39ff9f24b
Roberto Javier Homs Puron <rhoms@cells.es>
2019-02-19 16:02:40 +0100

    Bump version 2.7.2 to 2.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '2.7.2' 
+__version = '2.8.0' 
 
 # windows installer:
 # python setup.py bdist_wininst
