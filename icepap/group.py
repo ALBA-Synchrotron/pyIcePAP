456c6016085a76ad4c2dbd381622cd502829d149
Jose Tiago Macara Coutinho <tcoutinho@cells.es>
2020-11-11 18:43:12 +0100

    Robust group queries

--- icepap/group.py
+++ icepap/group.py
@@ -2,6 +2,33 @@ import time
 import collections.abc
 
 from .axis import IcePAPAxis
+from .utils import State
+
+
+def get_ctrl_item(f, axes, default=None):
+    try:
+        return f(axes)
+    except Exception:
+        pass
+    values = []
+    for axis in axes:
+        try:
+            value = f(axis)[0]
+        except RuntimeError:
+            value = default
+        values.append(value)
+    return values
+
+
+def get_item(motors, name, default=None):
+    values = []
+    for motor in motors:
+        try:
+            value = getattr(motor, name)
+        except RuntimeError:
+            value = default
+        values.append(value)
+    return values
 
 
 class Group:
@@ -31,31 +58,31 @@ class Group:
 
     @property
     def axes(self):
-        return [motor.axis for motor in self._motors]
+        return [motor.axis for motor in self.motors]
 
     def get_names(self):
-        return [motor.name for motor in self._motors]
+        return get_item(self.motors, "name")
 
     def get_acctime(self):
-        return self._controller.get_acctime(self.axes)
+        return get_ctrl_item(self.controller.get_acctime, self.axes)
 
     def get_velocity(self):
-        return self._controller.get_velocity(self.axes)
+        return get_ctrl_item(self.controller.get_velocity, self.axes)
 
     def get_pos(self):
-        return self._controller.get_pos(self.axes)
+        return get_ctrl_item(self.controller.get_pos, self.axes)
 
     def get_fpos(self):
-        return self._controller.get_fpos(self.axes)
+        return get_ctrl_item(self.controller.get_fpos, self.axes)
 
     def get_states(self):
-        return self._controller.get_states(self.axes)
+        return get_ctrl_item(self.controller.get_states, self.axes, State(0))
 
     def is_moving(self):
         return any(state.is_moving() for state in self.get_states())
 
     def get_power(self):
-        return self._controller.get_power(self.axes)
+        return get_ctrl_item(self.controller.get_power, self.axes)
 
     def start_stop(self):
         self._controller.stop(self.axes)
