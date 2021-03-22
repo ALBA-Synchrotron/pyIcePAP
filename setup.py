3edd399958eb4162354c6ad15fea2b123c8d2449
Roberto Javier Homs Puron <rhoms@cells.es>
2021-03-22 15:07:54 +0100

    Bump version 3.5.0 to 3.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '3.5.0'
+__version = '3.5.1'
 
 # windows installer:
 # python setup.py bdist_wininst
