02bf4cada740b03624a3e315ae711e4d65747249
Roberto Javier Homs Puron <rhoms@cells.es>
2021-04-30 18:07:43 +0200

    Add license

--- icepap/tools.py
+++ icepap/tools.py
@@ -1,3 +1,15 @@
+# -----------------------------------------------------------------------------
+# This file is part of icepap (https://github.com/ALBA-Synchrotron/pyIcePAP)
+#
+# Copyright 2008-2017 CELLS / ALBA Synchrotron, Bellaterra, Spain
+#
+# Distributed under the terms of the GNU General Public License,
+# either version 3 of the License, or (at your option) any later version.
+# See LICENSE.txt for more info.
+#
+# You should have received a copy of the GNU General Public License
+# along with icepap. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 import os
 import time
 import signal
