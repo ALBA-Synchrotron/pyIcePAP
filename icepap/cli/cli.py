4053760d77aa993c71f1f5b4291673e36f9df6e8
Roberto Javier Homs Puron <rhoms@cells.es>
2022-09-29 21:54:18 +0200

    Refactor names

--- icepap/cli/cli.py
+++ icepap/cli/cli.py
@@ -46,11 +46,11 @@ class Axes(click.ParamType):
     name = "axes"
 
     def convert(self, value, param, ctx):
-        pap = ctx.obj["icepap"]
+        ipap = ctx.obj["icepap"]
         if value == "all":
-            axes = pap.find_axes(only_alive=False)
+            axes = ipap.find_axes(only_alive=False)
         elif value == "alive":
-            axes = pap.find_axes(only_alive=True)
+            axes = ipap.find_axes(only_alive=True)
         else:
             axes = []
             for axis in value.split(","):
@@ -59,7 +59,7 @@ class Axes(click.ParamType):
                 except ValueError:
                     axes.append(axis.strip())
             axes.sort()
-        return pap[axes]
+        return ipap[axes]
 
 
 class Racks(click.ParamType):
@@ -136,14 +136,14 @@ opt_mandatory_axes = click.option(
     help="comma separated list of axes. Also supports 'all' and 'alive'"
 )
 
-opt_rack = click.option(
+opt_mandatory_rack = click.option(
     "--rack", type=int, default=None, show_default=True,
     help="rack number. Default is None, meaning the whole system"
 )
 
-opt_racks = click.option(
-    "--racks", type=Racks, required=True, show_default=True,
-    help="comma separated list of racks. Also supports 'all' and 'alive'"
+opt_mandatory_racks = click.option(
+    "--racks", type=Racks(), required=True, show_default=True,
+    help="comma separated list of racks. Also supports 'all'"
 )
 
 opt_racks = click.option(
@@ -180,8 +180,8 @@ def move(ctx, pairs, pb_format, bottom_toolbar, title):
     Move specified axes in pairs <axis, position> to the specified absolute
     positions.
     """
-    pap = ctx.obj["icepap"]
-    motors = [pap[int(address)] for address in pairs[::2]]
+    ipap = ctx.obj["icepap"]
+    motors = [ipap[int(address)] for address in pairs[::2]]
     positions = [int(position) for position in pairs[1::2]]
     cli_move(Group(motors), positions, pb_format, bottom_toolbar, title)
 
@@ -198,8 +198,8 @@ def rmove(ctx, pairs, pb_format, bottom_toolbar, title, multiple):
     Move specified axes in pairs <axis, position> relative to their current
     positions.
     """
-    pap = ctx.obj["icepap"]
-    motors = [pap[int(address)] for address in pairs[::2]]
+    ipap = ctx.obj["icepap"]
+    motors = [ipap[int(address)] for address in pairs[::2]]
     deltas = [int(position) for position in pairs[1::2]]
     cli_rmove(
         Group(motors),
@@ -275,8 +275,8 @@ def ver():
 @click.pass_context
 def ver_system(ctx):
     """Prints a summary of icepap version"""
-    pap = ctx.obj["icepap"]
-    click.echo(pap.ver)
+    ipap = ctx.obj["icepap"]
+    click.echo(ipap.ver)
 
 
 @ver.command('axes')
@@ -294,20 +294,20 @@ def ver_axes(ctx, motors, info, table_style):
 @click.pass_context
 def mode(ctx):
     """Prints the operation mode"""
-    pap = ctx.obj["icepap"]
-    click.echo(pap.mode)
+    ipap = ctx.obj["icepap"]
+    click.echo(ipap.mode)
 
 
 @cli.command()
-@opt_rack
+@opt_mandatory_rack
 @click.confirmation_option(
     prompt="Are you sure you want to reset the rack(s)?"
 )
 @click.pass_context
 def reset(ctx, rack):
     """Resets the given rack. If no rack is given, resets the whole system"""
-    pap = ctx.obj["icepap"]
-    pap.reset(rack)
+    ipap = ctx.obj["icepap"]
+    ipap.reset(rack)
 
 
 @cli.command()
@@ -316,9 +316,9 @@ def reset(ctx, rack):
 )
 @click.pass_context
 def reboot(ctx):
-    "Reboots the icepap"
-    pap = ctx.obj["icepap"]
-    pap.reboot()
+    """Reboots the icepap"""
+    ipap = ctx.obj["icepap"]
+    ipap.reboot()
 
 
 @cli.command()
@@ -327,9 +327,9 @@ def reboot(ctx):
 @click.pass_context
 def rinfo(ctx, racks, table_style):
     """Prints a summary of each rack information in form of table"""
-    pap = ctx.obj["icepap"]
-    rids = pap.get_rid(racks)
-    temps = pap.get_rtemp(racks)
+    ipap = ctx.obj["icepap"]
+    rids = ipap.get_rid(racks)
+    temps = ipap.get_rtemp(racks)
     table = Table(style=table_style)
     table.columns.header = ("Rack #", "RID", "Temp.")
     for row in zip(racks, rids, temps):
@@ -342,9 +342,9 @@ def rinfo(ctx, racks, table_style):
 @click.pass_context
 def raw(ctx, cmd):
     """Send raw command to the icepap"""
-    pap = ctx.obj['icepap']
+    ipap = ctx.obj['icepap']
     cmd = ' '.join(cmd)
-    output = pap.send_cmd(cmd)
+    output = ipap.send_cmd(cmd)
     if output is not None:
         for line in output:
             click.echo(line)
